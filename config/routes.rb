2433800770147b76b10628f564ce31699ebf487a
Tammy <tammyfaga@yahoo.com.br>
2019-02-04 23:36:39 -0200

    funcionalidade: favoritar musicas, albuns e artistas

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
-  get 'albums/show'
+  get 'recently_heards/create'
   devise_for :users
-
+  
   authenticated :user do
     root to: "dashboard#index", as: :authenticated_root
     resources :search, only: [:index, :new], as: :searches
     resources :categories, only: :show
     resources :artists, only: :show
-    resources :albums, only: :show
+    resources :albums, only: :show do
+      resources :recently_heards, only: :create
+    end
+
+    resources :favorites, only: :index
+    resources :songs, only: [] do
+      post "/favorite", to: "favorites#create", on: :member, defaults: { format: :js, favoritable_type: 'Song' }
+      delete "/favorite", to: "favorites#destroy", on: :member, defaults: { format: :js, favoritable_type: 'Song' }
+    end
   end
 
   unauthenticated :user do
